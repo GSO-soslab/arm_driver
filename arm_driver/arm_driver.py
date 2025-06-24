831778e416a8490d36934db74808f3959b16efec
Kylie-URI <kyliepasternak@uri.edu>
2025-06-24 20:00:08 +0000

    better version

--- arm_driver/arm_driver.py
+++ arm_driver/arm_driver.py
@@ -26,20 +26,28 @@ class Arm_Driver(Node):
 
     def setup_params(self):
 
+        # Add motor parameters
+
         servo_ch_list_descriptor = ParameterDescriptor(description='List of servo channels for docking arm!')
         self.declare_parameter('servo_ch_list',[1], servo_ch_list_descriptor)
 
         servo_topic_list_descriptor = ParameterDescriptor(description='List of servo topics for docking arm!')
         self.declare_parameter('servo_topic_list',['servo_#'], servo_topic_list_descriptor)
 
-        angle_min_deg_descriptor = ParameterDescriptor(description='Minimum angle of servo in degrees!')
-        self.declare_parameter('angle_min_deg',[1], angle_min_deg_descriptor)
+        # angle_hard_min_deg_descriptor = ParameterDescriptor(description='Harware minimum angle of servo in degrees!')
+        # self.declare_parameter('angle_hard_min_deg',[1], angle_hard_min_deg_descriptor)
 
-        angle_max_deg_descriptor = ParameterDescriptor(description='Maximum angle of servo in degrees!')
-        self.declare_parameter('angle_max_deg',[1], angle_max_deg_descriptor)
+        # angle_hard_max_deg_descriptor = ParameterDescriptor(description='Harware maximum angle of servo in degrees!')
+        # self.declare_parameter('angle_hard_max_deg',[1], angle_hard_max_deg_descriptor)
+
+        angle_soft_min_deg_descriptor = ParameterDescriptor(description='Software limited minimum angle of servo in degrees!')
+        self.declare_parameter('angle_soft_min_deg',[1], angle_soft_min_deg_descriptor)
+
+        angle_max_deg_descriptor = ParameterDescriptor(description='Software limited maximum angle of servo in degrees!')
+        self.declare_parameter('angle_soft_max_deg',[1], angle_max_deg_descriptor)
         
-        angle_init_deg_descriptor = ParameterDescriptor(description='Initial angle (in degrees) to position servo!')
-        self.declare_parameter('angle_init_deg',[1], angle_init_deg_descriptor)
+        angle_offset_descriptor = ParameterDescriptor(description='Servo Angle offset (in degrees)!')
+        self.declare_parameter('angle_offset',[1], angle_offset_descriptor)
 
         angle_equation_c1_descriptor = ParameterDescriptor(description='First coefficient of servo angle equation!')
         self.declare_parameter('angle_equation_c1',[1.0], angle_equation_c1_descriptor)
@@ -49,16 +57,26 @@ class Arm_Driver(Node):
 
         self.servo_ch_list = list(self.get_parameter('servo_ch_list').get_parameter_value().integer_array_value)
         self.servo_topic_list = list(self.get_parameter('servo_topic_list').get_parameter_value().string_array_value)
-        self.angle_min_deg = list(self.get_parameter('angle_min_deg').get_parameter_value().integer_array_value)
-        self.angle_max_deg = list(self.get_parameter('angle_max_deg').get_parameter_value().integer_array_value)
-        self.angle_init_deg = list(self.get_parameter('angle_init_deg').get_parameter_value().integer_array_value)
+        # self.angle_hard_min_deg = list(self.get_parameter('angle_hard_min_deg').get_parameter_value().integer_array_value)
+        # self.angle_hard_max_deg = list(self.get_parameter('angle_hard_max_deg').get_parameter_value().integer_array_value)
+        self.angle_soft_min_deg = list(self.get_parameter('angle_soft_min_deg').get_parameter_value().integer_array_value)
+        self.angle_soft_max_deg = list(self.get_parameter('angle_soft_max_deg').get_parameter_value().integer_array_value)
+        self.angle_offset = list(self.get_parameter('angle_offset').get_parameter_value().integer_array_value)
         self.angle_equation_c1 = list(self.get_parameter('angle_equation_c1').get_parameter_value().double_array_value)
         self.angle_equation_c2 = list(self.get_parameter('angle_equation_c2').get_parameter_value().double_array_value)
-       
-       
-       
-       
-        # Adding pwm parameters
+
+        self.get_logger().info(f'servo_ch_list: {self.servo_ch_list}', once = True)
+        self.get_logger().info(f'servo_topic_list: {self.servo_topic_list}', once = True) 
+        # self.get_logger().info(f'angle_hard_min_deg: {self.angle_hard_min_deg}', once = True)
+        # self.get_logger().info(f'angle_hard_max_deg: {self.angle_hard_max_deg}', once = True)
+        self.get_logger().info(f'angle_soft_min_deg: {self.angle_soft_min_deg}', once = True)
+        self.get_logger().info(f'angle_soft_max_deg: {self.angle_soft_max_deg}', once = True)
+        self.get_logger().info(f'angle_offset {self.angle_offset}', once = True)
+        self.get_logger().info(f'angle_equation_c1: {self.angle_equation_c1}', once = True)
+        self.get_logger().info(f'angle_equation_c2={self.angle_equation_c2}', once = True)
+
+
+        # Add pwm parameters to step motor slower/faster
 
         current_pwm_descriptor = ParameterDescriptor(description='Current PWM values for each servo')
         self.declare_parameter('current_pwm', [0.0] * len(self.servo_topic_list), current_pwm_descriptor)
@@ -72,26 +90,16 @@ class Arm_Driver(Node):
         timer_interval_descriptor = ParameterDescriptor(description='Interval in seconds between timer callbacks')
         self.declare_parameter('timer_interval', 0.1, timer_interval_descriptor)
 
-
-        
-
-        # Adding PWM Parameters
         self.current_pwm = list(self.get_parameter('current_pwm').get_parameter_value().double_array_value)
         self.desired_pwm = list(self.get_parameter('desired_pwm').get_parameter_value().double_array_value)
         self.steps = self.get_parameter('steps').get_parameter_value().double_value
         self.timer_interval = self.get_parameter('timer_interval').get_parameter_value().double_value
 
-        self.get_logger().info(f'servo_ch_list: {self.servo_ch_list}', once = True)
-        self.get_logger().info(f'servo_topic_list: {self.servo_topic_list}', once = True)
-        self.get_logger().info(f'angle_min_deg: {self.angle_min_deg}', once = True)
-        self.get_logger().info(f'angle_max_deg: {self.angle_max_deg}', once = True)
-        self.get_logger().info(f'angle_init_deg: {self.angle_init_deg}', once = True)
-        self.get_logger().info(f'angle_equation_c1: {self.angle_equation_c1}', once = True)
-        self.get_logger().info(f'angle_equation_c2={self.angle_equation_c2}', once = True)
         self.get_logger().info(f'current_pwm={self.current_pwm}', once = True)
         self.get_logger().info(f'desired_pwm={self.desired_pwm}', once = True)
         self.get_logger().info(f'steps={self.steps}', once = True)
         self.get_logger().info(f'timer_interval={self.timer_interval}', once = True)
+
 
 
     def setup_ros(self):
@@ -115,34 +123,28 @@ class Arm_Driver(Node):
             )
             self.output_cmd_publishers[i] = output_cmd_pub
 
-        # self.input_angle_msg = Float64()
 
 
 
-    def input_angle_callback(self, msg, id):
-
+    def input_angle_callback(self, msg, id): 
+        print("input_angle_callback started")
         if 0 <= id < len(self.servo_topic_list):
             input_angle = msg.data
+            min_limit = self.angle_soft_min_deg[id]
+            max_limit = self.angle_soft_max_deg[id]
+            ang_offset = self.angle_offset[id]
+            if input_angle > max_limit:
+                input_angle = max_limit
+            if input_angle < min_limit:
+                input_angle = min_limit
             m = self.angle_equation_c1[id]  
             b = self.angle_equation_c2[id] 
-            output_cmd = m * input_angle + b
-
-            #self.get_logger().info(f"Calculated output for servo {id} (Topic: {self.servo_topic_list[id]}): {output_cmd}")
-            #self.get_logger().info(f"Received input angle: {msg.data}, Published to: {self.servo_topic_list[id]}, Calculated output cmd: {output_cmd}")
-            
-            # # If MZ wants publisher inside this function, uncomment next 3 lines
-            # servo_topic = self.servo_topic_list[id]
-            # output_cmd_pub = self.create_publisher(Float64, servo_topic, qos_profile=QoSProfile(depth=10))
-            # output_cmd_pub.publish(Float64(data=output_cmd))
-        # else:
-        #     self.get_logger().error(f"Invalid servo ID: {id}")
-
-        # New addition trying to implement timer
+            output_cmd = m * (input_angle + ang_offset) + b
             self.desired_pwm[id] = output_cmd
-
-            #self.get_logger().info(f"Received input angle: {msg.data}, Published to: {self.servo_topic_list[id]}, Calculated output cmd: {output_cmd}")
         else:
             self.get_logger().error(f"Invalid servo ID: {id}")    
+
+
         
     def command_pub_callback(self):
         # Update the PWM values gradually for each servo
@@ -159,8 +161,6 @@ class Arm_Driver(Node):
             # Publish the current PWM value to the respective servo topic
             self.output_cmd_publishers[i].publish(Float64(data=self.current_pwm[i]))
            # self.get_logger().info(f"Servo {i}: Current PWM = {self.current_pwm[i]}")
-
-# END ADDITIONS TO SLOW MOTOR
 
                                    
 def main(args=None):
